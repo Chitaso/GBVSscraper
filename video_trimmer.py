e2dbfcb60908a3f249ad68484aadf30b9194ee9e
a <>
2023-07-08 16:41:34 -0400

    temp2

--- video_trimmer.py
+++ video_trimmer.py
@@ -1,13 +1,14 @@
 import os
 import cv2
 import ffmpeg
+import numpy as np
 
 os.makedirs(os.path.abspath(f"{__file__}/../trimmed_videos"), exist_ok=True)
 os.makedirs(os.path.abspath(f"{__file__}/../ending_anim"), exist_ok=True)
 
 
 def is_frame_black(arr):
-    return not (arr > 7).any()
+    return not (arr > 10).any()
 
 
 def get_video_bounds(file_path):
@@ -34,13 +35,20 @@ def get_video_bounds(file_path):
     end_frame = -1
     end_frame_flag = False
 
+    black_frame_end_flag = False
+    black_frame_end = -1
+
     validate_end = [
-        [[436, 545], [92, 198, 247]],
-        [[973, 618], [15, 33, 55]],
-        [[1113, 539], [150, 255, 253]],
-        [[1280, 529], [137, 255, 250]],
-        [[1463, 472], [92, 184, 247]],
+        # Lose - Win
+        [[[204, 151], [135, 126, 123]], [[226, 229], [255, 250, 254]], [[280, 221], [40, 30, 37]], [[405, 194], [255, 251, 253]], [[450, 175], [205, 184, 196]],
+         [[1424, 169], [92, 192, 248]], [[1488, 218], [98, 181, 235]], [[1560, 205], [75, 165, 226]], [[1672, 188], [173, 255, 253]]],
+        # Win - Lose
+        [[[234, 151], [84, 183, 247]], [[302, 218], [101, 191, 241]], [[379, 192], [146, 208, 255]], [[414, 175], [71, 175, 230]], [[1384, 166], [77, 64, 72]],
+         [[1409, 223], [85, 73, 80]], [[1470, 224], [36, 26, 33]], [[1588, 191], [240, 222, 230]], [[1653, 191], [64, 55, 60]]]
     ]
+    for a in validate_end:
+        for b, c in enumerate(a):
+            a[b][1] = np.array(c[1])
 
     final_anim_frame = -1
     final_anim_flag = False
@@ -68,32 +76,56 @@ def get_video_bounds(file_path):
                     print(start_frame)
 
         if start_frame != -1 and not end_frame_flag:
-            flag = False
-            for i, j in validate_end:
-                p = img[i[1]][i[0]]
-                for k in range(3):
-                    if abs(p[k] - j[k]) > 25:
-                        flag = True
-                        break
-
-                if flag:
-                    break
+            if is_frame_black(img):
+                if not black_frame_end_flag:
+                    black_frame_end_flag = True
+                    black_frame_end = fno
             else:
-                print(fno)
-                end_frame_flag = True
+                black_frame_end_flag = False
 
-        if end_frame_flag and not final_anim_flag:
+        if black_frame_end != -1 and not end_frame_flag:
+            for a in validate_end:
+                for [pix, color] in a:
+                    p = img[pix[1]][pix[0]]
+                    if not (np.abs(color - p) < 15).all():
+                        break
+                else:
+                    print(fno)
+                    end_frame_flag = True
+                    end_frame = black_frame_end - 1
+
+            # flag = False
+            # for i, j in validate_end:
+            #     p = img[i[1]][i[0]]
+            #     for k in range(3):
+            #         if abs(p[k] - j[k]) > 25:
+            #             flag = True
+            #             break
+            #
+            #     if flag:
+            #         break
+            # else:
+            #     print(fno)
+            #     end_frame_flag = True
+
+        # if end_frame_flag and not final_anim_flag:
+        #     if is_frame_black(img):
+        #         end_frame = fno - 1
+        #         final_anim_flag = True
+        #
+        # if final_anim_flag and not final_anim_flag1:
+        #     if not is_frame_black(img):
+        #         final_anim_flag1 = True
+        #
+        # if final_anim_flag1:
+        #     if is_frame_black(img):
+        #         final_anim_frame = fno - 265
+        #         break
+        #
+
+        if end_frame_flag:
             if is_frame_black(img):
-                end_frame = fno - 1
-                final_anim_flag = True
-
-        if final_anim_flag and not final_anim_flag1:
-            if not is_frame_black(img):
-                final_anim_flag1 = True
-
-        if final_anim_flag1:
-            if is_frame_black(img):
-                final_anim_frame = fno - 265
+                final_anim_frame = fno - 268
                 break
 
         success, img = cap.read()
@@ -122,8 +154,12 @@ def trim_video(file_path):
         ffmpeg.input(file_path, ss=start_time, to=end_time, **kwargs).output(output_path).run(overwrite_output=True)
 
 
-# path = os.path.abspath(f"{__file__}/../videos")
-# for i in os.listdir(path):
-#     trim_video(f"{path}/{i}")
+path = os.path.abspath(f"{__file__}/../videos")
+blacklist = os.listdir(f"{__file__}/../trimmed_videos")
 
-trim_video(os.path.abspath(f"{__file__}/../videos/1--SEG overraled--Cagliostro--Inno--Percival.mp4"))
+for i in os.listdir(path):
+    if i not in blacklist:
+        print(f"Trimming video: {i}")
+        trim_video(f"{path}/{i}")
+
+# trim_video(os.path.abspath(f"{__file__}/../videos/12--jnh--Soriz--Prometheus--Avatar Belial.mp4"))
